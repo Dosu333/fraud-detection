c0203487314bcb2dbdf27144ef45b76388e1a2bc
Dosu333 <vladamir1865@gmail.com>
2025-10-08 13:06:52 +0100

    Add .env.example for environment variable configuration, update .gitignore to exclude .env and fraud_model.pkl, and modify main.py to load model path from environment variable. Add fraud_model_pipeline_v1.pkl to the model directory.

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from app.preprocess import FeatureEngineer
 from app.endpoints import router
 import joblib
 import os
 
+load_dotenv()
+
 app = FastAPI()
 
 @app.on_event("startup")
 def load_model():
-    MODEL_PATH = os.path.join(os.path.dirname(__file__), "../model/fraud_model_pipeline_v1.pkl")
+    model_name = os.environ.get('MODEL_NAME')
+    MODEL_PATH = os.path.join(os.path.dirname(__file__), f"../model/{model_name}")
     app.state.model = joblib.load(MODEL_PATH)
     print("Model loaded successfully.")
     
