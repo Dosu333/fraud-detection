88a7ed4e7c2e0d7a79ccdf503ff3f74a9fc78315
Dosu333 <vladamir1865@gmail.com>
2025-10-08 14:37:50 +0100

    Add logging functionality to endpoints and model loading in main.py. Measure and log processing time for predictions and model loading, enhancing observability of the application.

--- app/endpoints.py
+++ app/endpoints.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Request
 from app.models import Transaction
+from app.logger import logger
 import pandas as pd
+import time
+
 
 router = APIRouter()
 
@@ -10,11 +13,19 @@ def root():
 
 @router.post("/predict/")
 def predict(request: Request, input_data: Transaction):
+    start_time = time.time()
     model = request.app.state.model
     df = pd.DataFrame([input_data.dict()])
     prediction = model.predict(df)[0]
     prob = model.predict_proba(df)[0][1]
+    elapsed = round(time.time() - start_time, 3)
+
+    logger.info(
+        f"Prediction completed in {elapsed}s | Fraud Probability: {prob:.4f} | Prediction: {bool(prediction)}"
+    )
+
     return {
         "prediction": bool(prediction),
-        "fraud_probability": f"{(float(prob) * 100):.2f}"
+        "fraud_probability": float(round(prob, 4)),
+        "processing_time": elapsed,
     }
--- app/main.py
+++ app/main.py
@@ -2,18 +2,23 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 from app.preprocess import FeatureEngineer
 from app.endpoints import router
+from app.logger import logger
 import joblib
+import time
 import os
 
+# Load .env
 load_dotenv()
 
 app = FastAPI()
 
 @app.on_event("startup")
 def load_model():
+    start_time = time.time()
     model_name = os.environ.get('MODEL_NAME')
     MODEL_PATH = os.path.join(os.path.dirname(__file__), f"../model/{model_name}")
     app.state.model = joblib.load(MODEL_PATH)
-    print("Model loaded successfully.")
+    elapsed = round(time.time() - start_time, 2)
+    logger.info(f"✅ Model loaded successfully in {elapsed}s")
     
 app.include_router(router)
