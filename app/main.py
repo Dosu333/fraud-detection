2cc63e8badf87ded48f048bdcb076a45b5f00058
Dosu333 <vladamir1865@gmail.com>
2025-10-30 13:23:14 +0100

    Update .gitignore to include .pkl files; refactor main.py for improved readability and model loading

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from app.preprocess import FeatureEngineer
 from app.endpoints import router
 from app.logger import logger
 import joblib
@@ -10,30 +9,29 @@ import os
 # Load .env
 load_dotenv()
 
-from fastapi import FastAPI
 
 app = FastAPI(
-    title="Online Fraud Detection API",   
-    description="This API detects fraudulent transactions using a machine learning model.",
+    title="Online Fraud Detection API",
+    description="""This API detects fraudulent transactions
+                    using a machine learning model.""",
     version="1.0.0",
     contact={
         "name": "Oladosu Larinde",
         "email": "larindeakin@gmail.com",
     },
-    license_info={
-        "name": "MIT",
-        "url": "https://opensource.org/licenses/MIT"
-    }
+    license_info={"name": "MIT", "url": "https://opensource.org/licenses/MIT"},
 )
 
 
 @app.on_event("startup")
 def load_model():
     start_time = time.time()
-    model_name = os.environ.get('MODEL_NAME')
-    MODEL_PATH = os.path.join(os.path.dirname(__file__), f"../model/{model_name}")
+    model_name = os.environ.get("MODEL_NAME")
+    MODEL_PATH = os.path.join(
+        os.path.dirname(__file__), f"../model/{model_name}")
     app.state.model = joblib.load(MODEL_PATH)
     elapsed = round(time.time() - start_time, 2)
     logger.info(f"✅ Model loaded successfully in {elapsed}s")
-    
+
+
 app.include_router(router)
