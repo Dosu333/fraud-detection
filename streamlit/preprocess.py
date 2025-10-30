d6a0c835e7da59043e08cf7ccf2c37eb9a49b41d
Dosu333 <vladamir1865@gmail.com>
2025-10-30 13:22:43 +0100

    Enhance FeatureEngineer class in preprocessing; add timestamp handling and new feature extraction for improved data transformation.

--- streamlit/preprocess.py
+++ streamlit/preprocess.py
@@ -18,9 +18,16 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
 
     def transform(self, X):
         X = X.copy()
-
-        for col in ['amount', 'oldbalanceOrg']:
-            X[col] = np.log1p(X[col])
+        print(list(X.columns))
+        for col in [
+            "amount",
+            "oldbalanceOrg",
+            "avgDailyVolumeSoFar",
+            "avgDailyVolumeBeforeTxn",
+            "amountToAvgVolumeRatio",
+        ]:
+            if col in X.columns:
+                X[col] = np.log1p(X[col].astype(float))
 
         if 'step' in X.columns:
             X['hourOfDay'] = X['step'].apply(
@@ -32,10 +39,22 @@ class FeatureEngineer(BaseEstimator, TransformerMixin):
             X = X.drop(['step'], axis=1)
 
         if 'timestamp' in X.columns:
-            X['timestamp'] = pd.to_datetime(X['timestamp'], unit='s')
+            X['timestamp'] = X['timestamp'].astype(str)
+
+            if X['timestamp'].str.isnumeric().all():
+                X['timestamp'] = pd.to_datetime(
+                    X['timestamp'].astype(float), unit='s', errors='coerce')
+            else:
+                X['timestamp'] = pd.to_datetime(
+                    X['timestamp'], errors='coerce')
+
             X['hourOfDay'] = X['timestamp'].dt.hour
             X['dayOfMonth'] = X['timestamp'].dt.day
             X['isWeekend'] = X['timestamp'].dt.weekday.apply(
-                                    lambda x: 1 if x >= 5 else 0)
+                lambda x: 1 if x >= 5 else 0)
             X = X.drop(['timestamp'], axis=1)
+
+        X['type_CASH_OUT'] = (X['type'] == 'CASH_OUT').astype(int)
+        X['type_TRANSFER'] = (X['type'] == 'TRANSFER').astype(int)
+        X = X.drop(['type'], axis=1)
         return X
